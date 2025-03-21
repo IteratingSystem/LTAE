b2973dc5b2c64b2670cb2be9b40cd248a4339330
Wenlong <midsu_wy@163.com>
2025-03-21 11:23:55 +0800

    从tiled中获取翻转状态

--- src/main/java/org/ltae/component/Render.java
+++ src/main/java/org/ltae/component/Render.java
@@ -27,6 +27,19 @@ public class Render extends Component implements TileCompLoader {
         MapObject mapObject = tileDetails.mapObject;
         if (mapObject instanceof TextureMapObject textureMapObject) {
             keyFrame = textureMapObject.getTextureRegion();
+
+
+            //是否翻转
+            MapProperties properties = textureMapObject.getProperties();
+            boolean flipX = false;
+            boolean flipY = false;
+            if (properties.get("flipHorizontally")!=null){
+                flipX = properties.get("flipHorizontally",boolean.class);
+            }
+            if (properties.get("flipVertically")!=null){
+                flipY = properties.get("flipVertically",boolean.class);
+            }
+            keyFrame.flip(flipX,flipY);
         }
     }
 }
