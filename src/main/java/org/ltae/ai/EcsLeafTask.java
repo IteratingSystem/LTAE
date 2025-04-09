173cb53a03f40da26dce75f0b34d447855b617b8
Wenlong <midsu_wy@163.com>
2025-04-09 17:04:33 +0800

    创建叶任务类

---
+++ src/main/java/org/ltae/ai/EcsLeafTask.java
@@ -0,0 +1,46 @@
+package org.ltae.ai;
+
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.World;
+import com.artemis.managers.TagManager;
+import com.badlogic.gdx.ai.btree.LeafTask;
+import com.badlogic.gdx.ai.btree.Task;
+
+/**
+ * @Auther WenLong
+ * @Date 2025/4/9 16:59
+ * @Description ecs叶任务
+ **/
+public class EcsLeafTask extends LeafTask<Entity> {
+
+    public World world;
+    public Entity entity;
+    public int entityId;
+    public TagManager tagManager;
+    private void initialize(){
+        entity = getObject();
+        world = entity.getWorld();
+        entityId = entity.getId();
+        tagManager = world.getSystem(TagManager.class);
+    }
+
+    @Override
+    public void start() {
+        initialize();
+    }
+
+    @Override
+    protected Task<Entity> copyTo(Task<Entity> task) {
+        return task;
+    }
+
+    @Override
+    public Status execute() {
+        return Status.RUNNING;
+    }
+    public final String getTAG(){
+        return getClass().getSimpleName();
+    }
+}
+
