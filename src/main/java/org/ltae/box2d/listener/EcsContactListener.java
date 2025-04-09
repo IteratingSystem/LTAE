16afc3603a0c73d0e50a17b63d434b38dbfa1287
Wenlong <midsu_wy@163.com>
2025-04-09 14:48:40 +0800

    完成帧形状功能

--- src/main/java/org/ltae/box2d/listener/EcsContactListener.java
+++ src/main/java/org/ltae/box2d/listener/EcsContactListener.java
@@ -2,10 +2,8 @@ package org.ltae.box2d.listener;
 
 import com.artemis.Entity;
 import com.artemis.World;
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.physics.box2d.*;
+import org.ltae.box2d.DefFixData;
 
 /**
  * @Auther WenLong
@@ -13,11 +11,18 @@ import com.badlogic.gdx.physics.box2d.Manifold;
  * @Description 包含实体数据的监听器,需要注意的是,传感器只会触发beginContact和endContact回调
  **/
 public class EcsContactListener implements ContactListener {
+    public int entityId;
     public Entity entity;
     public World world;
+
+    public Fixture thisFix;
+    public Fixture otherFix;
+    public Object thisFixData;
+    public Object otherFixData;
     public EcsContactListener(Entity entity){
         this.entity = entity;
         world = entity.getWorld();
+        entityId = entity.getId();
     }
 
     /**
@@ -26,7 +31,36 @@ public class EcsContactListener implements ContactListener {
      */
     @Override
     public void beginContact(Contact contact) {
+        Fixture fixtureA = contact.getFixtureA();
+        Fixture fixtureB = contact.getFixtureB();
 
+        Object userDataA = fixtureA.getUserData();
+        Object userDataB = fixtureB.getUserData();
+        if (userDataA instanceof DefFixData defFixData) {
+            if (defFixData.entityId == entityId) {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }else {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }
+        }else if (userDataB instanceof DefFixData defFixData){
+            if (defFixData.entityId == entityId) {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }else {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }
+        }
     }
 
     /**
@@ -35,7 +69,36 @@ public class EcsContactListener implements ContactListener {
      */
     @Override
     public void endContact(Contact contact) {
+        Fixture fixtureA = contact.getFixtureA();
+        Fixture fixtureB = contact.getFixtureB();
 
+        Object userDataA = fixtureA.getUserData();
+        Object userDataB = fixtureB.getUserData();
+        if (userDataA instanceof DefFixData defFixData) {
+            if (defFixData.entityId == entityId) {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }else {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }
+        }else if (userDataB instanceof DefFixData defFixData){
+            if (defFixData.entityId == entityId) {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }else {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }
+        }
     }
 
     /**
@@ -45,7 +108,36 @@ public class EcsContactListener implements ContactListener {
      */
     @Override
     public void preSolve(Contact contact, Manifold manifold) {
+        Fixture fixtureA = contact.getFixtureA();
+        Fixture fixtureB = contact.getFixtureB();
 
+        Object userDataA = fixtureA.getUserData();
+        Object userDataB = fixtureB.getUserData();
+        if (userDataA instanceof DefFixData defFixData) {
+            if (defFixData.entityId == entityId) {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }else {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }
+        }else if (userDataB instanceof DefFixData defFixData){
+            if (defFixData.entityId == entityId) {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }else {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }
+        }
     }
 
     /**
@@ -55,6 +147,35 @@ public class EcsContactListener implements ContactListener {
      */
     @Override
     public void postSolve(Contact contact, ContactImpulse contactImpulse) {
+        Fixture fixtureA = contact.getFixtureA();
+        Fixture fixtureB = contact.getFixtureB();
 
+        Object userDataA = fixtureA.getUserData();
+        Object userDataB = fixtureB.getUserData();
+        if (userDataA instanceof DefFixData defFixData) {
+            if (defFixData.entityId == entityId) {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }else {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }
+        }else if (userDataB instanceof DefFixData defFixData){
+            if (defFixData.entityId == entityId) {
+                otherFix = fixtureA;
+                otherFixData = defFixData;
+                thisFix = fixtureB;
+                thisFixData = fixtureB.getUserData();
+            }else {
+                thisFix = fixtureA;
+                thisFixData = defFixData;
+                otherFix = fixtureB;
+                otherFixData = fixtureB.getUserData();
+            }
+        }
     }
 }
