e4c31ec9a2fae09735c3e5efc19335e6f807463b
Wenlong <midsu_wy@163.com>
2025-06-18 17:41:06 +0800

    制作多语言管理器

---
+++ src/main/java/org/ltae/manager/BundleManager.java
@@ -0,0 +1,43 @@
+package org.ltae.manager;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.I18NBundleLoader;
+import com.badlogic.gdx.utils.I18NBundle;
+
+import java.util.Locale;
+
+
+/**
+ * @Auther WenLong
+ * @Date 2025/6/18 17:20
+ * @Description 多语言Bundle管理器
+ **/
+public class BundleManager {
+    private final static String TAG = BundleManager.class.getSimpleName();
+    private static I18NBundle bundle;
+    private static AssetManager assetManager;
+    private static String path;
+    public static void initialize(AssetManager assetManager,String path,String language){
+        BundleManager.assetManager = assetManager;
+        BundleManager.path = path;
+
+        I18NBundleLoader.I18NBundleParameter bundleParameter = new I18NBundleLoader.I18NBundleParameter(new Locale(language));
+        assetManager.load(path, I18NBundle.class,bundleParameter);
+        assetManager.finishLoading();
+        bundle = assetManager.get(path, I18NBundle.class);
+    }
+    public static void updateLanguage(String language){
+        I18NBundleLoader.I18NBundleParameter bundleParameter = new I18NBundleLoader.I18NBundleParameter(new Locale(language));
+        assetManager.load(path, I18NBundle.class,bundleParameter);
+        assetManager.finishLoading();
+        bundle = assetManager.get(path, I18NBundle.class);
+    }
+
+    public static I18NBundle getBundle(){
+        if (bundle == null){
+            Gdx.app.error(TAG,"bundle is null,please use BundleManager.initialize(AssetManager assetManager,String path)!");
+        }
+        return bundle;
+    }
+}
