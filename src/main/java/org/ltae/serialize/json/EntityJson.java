a368572bba8ecb89020e767cd7c42feaef5d58c6
Wenlong <midsu_wy@163.com>
2025-07-07 17:17:17 +0800

    尝试使用SaveFileFormat

--- src/main/java/org/ltae/serialize/json/EntityJson.java
+++ src/main/java/org/ltae/serialize/json/EntityJson.java
@@ -1,5 +1,6 @@
 package org.ltae.serialize.json;
 
+import com.artemis.io.SaveFileFormat;
 import com.artemis.utils.Bag;
 import com.badlogic.gdx.maps.MapObject;
 
@@ -8,12 +9,13 @@ import com.badlogic.gdx.maps.MapObject;
  * @Date 2025/7/4 10:26
  * @Description
  **/
-public class EntityJson {
+public class EntityJson extends SaveFileFormat {
     public int entityId;
     public String name;
     public String type;
     public Bag<ComponentJson> components;
     public MapObject mapObject;
+    public EntityJson(){}
 
     public boolean hasComp(String name){
         if (components == null){
