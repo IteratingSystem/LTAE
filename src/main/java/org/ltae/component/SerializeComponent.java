07325c7aaefa47ceb0d12fed9ec15da6aff3f158
Wenlong <midsu_wy@163.com>
2025-07-15 15:23:07 +0800

    尝试制作切换地图功能

--- src/main/java/org/ltae/component/SerializeComponent.java
+++ src/main/java/org/ltae/component/SerializeComponent.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.maps.tiled.objects.TiledMapTileMapObject;
 import org.ltae.LtaePluginRule;
 import org.ltae.manager.map.MapManager;
 import org.ltae.manager.map.serialize.json.EntityData;
+import org.ltae.system.TiledMapSystem;
 
 //组件继承于它可以自定义加载逻辑
 public abstract class SerializeComponent extends Component {
@@ -21,12 +22,17 @@ public abstract class SerializeComponent extends Component {
         this.world = world;
         entityId = entityData.entityId;
         int mapObjectId = entityData.mapObjectId;
+        TiledMapSystem tiledMapSystem = world.getSystem(TiledMapSystem.class);
+        String current = tiledMapSystem.getCurrent();
+
         MapManager mapManager = MapManager.getInstance();
-        MapObjects mapObjects = mapManager.getMapObjects(LtaePluginRule.MAP_NAME);
+        MapObjects mapObjects = mapManager.getMapObjects(current);
+
         for (MapObject mapObject_ : mapObjects) {
             Integer id = mapObject_.getProperties().get("id", -1, Integer.class);
             if (mapObjectId == id){
                 mapObject = mapObject_;
+                break;
             }
         }
         if (mapObject instanceof TiledMapTileMapObject tileMapObject) {
