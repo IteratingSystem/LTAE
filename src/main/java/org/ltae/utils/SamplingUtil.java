9c80b23abb8ea0b9e280b085d6468915bee7d308
WenLong <midsu_wy@163.com>
2025-10-15 11:45:43 +0800

    图层采样添加日志

--- src/main/java/org/ltae/utils/SamplingUtil.java
+++ src/main/java/org/ltae/utils/SamplingUtil.java
@@ -1,5 +1,6 @@
 package org.ltae.utils;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
@@ -14,6 +15,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
  * 图层采样工具
  */
 public class SamplingUtil {
+    private static final String TAG = SamplingUtil.class.getSimpleName();
     private static SamplingUtil instance;
     private FrameBuffer fbo;
     private OrthogonalTiledMapRenderer mapRenderer;
@@ -36,7 +38,15 @@ public class SamplingUtil {
         return samplingLayer(tiledMap,layerName,Texture.TextureFilter.Nearest,Texture.TextureFilter.Nearest);
     }
     public TextureRegion samplingLayer(TiledMap tiledMap,String layerName,Texture.TextureFilter minFilter, Texture.TextureFilter magFilter){
-        TiledMapTileLayer mapLayer = (TiledMapTileLayer)tiledMap.getLayers().get(layerName);
+        TiledMapTileLayer mapLayer = null;
+        try{
+            mapLayer = (TiledMapTileLayer)tiledMap.getLayers().get(layerName);
+        } catch (Exception e) {
+            Gdx.app.error(TAG,"Failed to samplingLayer,layerName is '"+layerName+"'");
+            throw new RuntimeException(e);
+        }
+
+
         int width = mapLayer.getWidth()*mapLayer.getTileWidth();
         int height = mapLayer.getHeight()*mapLayer.getTileHeight();
         fbo = new FrameBuffer(Pixmap.Format.RGBA8888, width, height, false);
