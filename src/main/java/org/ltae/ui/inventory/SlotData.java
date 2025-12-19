7709e9b3ad5491b2fd84fadab735b6b468cfd04c
WenLong <midsu_wy@163.com>
2025-12-19 08:02:14 +0800

    使用奇怪的方式获取图片

--- src/main/java/org/ltae/ui/inventory/SlotData.java
+++ src/main/java/org/ltae/ui/inventory/SlotData.java
@@ -1,9 +1,14 @@
 package org.ltae.ui.inventory;
 
+import com.artemis.ComponentManager;
+import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.World;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import net.mostlyoriginal.api.event.common.EventSystem;
+import org.ltae.component.Inert;
 import org.ltae.component.Render;
+import org.ltae.event.EntityEvent;
 import org.ltae.manager.map.serialize.json.EntityDatum;
 
 public class SlotData {
@@ -18,8 +23,16 @@ public class SlotData {
         if (entityDatum == null){
             return null;
         }
-        Entity entity = world.getEntity(entityDatum.entityId);
-        Render render = entity.getComponent(Render.class);
+
+        //暂时先用这种方式获取图片
+        EntityEvent entityEvent = new EntityEvent(EntityEvent.BUILD_ENTITY);
+        entityEvent.entityDatum = entityDatum;
+        EventSystem eventSystem = world.getSystem(EventSystem.class);
+        eventSystem.dispatch(entityEvent);
+        Render render = entityEvent.entity.getComponent(Render.class);
+        ComponentMapper<Inert> mInert = world.getMapper(Inert.class);
+        mInert.create(entityEvent.entity);
+
         return new TextureRegionDrawable(render.keyframe);
     }
 }
