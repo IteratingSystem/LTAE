0f92af124ac69b479d57c895d9a39f390792ab8f
WenLong <midsu_wy@163.com>
2025-12-21 07:41:46 +0800

    修复地图分裂问题

--- src/main/java/org/ltae/utils/TiledMapUtils.java
+++ src/main/java/org/ltae/utils/TiledMapUtils.java
@@ -2,8 +2,10 @@ package org.ltae.utils;
 
 import com.artemis.utils.Bag;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.*;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 import java.util.Iterator;
 
@@ -44,5 +46,42 @@ public class TiledMapUtils {
             mapObjects.addAll(objects);
         }
         return mapObjects;
+    }
+
+
+    //填充图像边缘区域,修复地图分裂问题
+    public static void fillSplit(TiledMap tiledMap){
+        //解决地图开裂的bug
+        for (MapLayer layer : tiledMap.getLayers()) {
+            if (layer instanceof TiledMapTileLayer tileLayer) {
+                for (int x = 0; x < tileLayer.getWidth(); x++) {
+                    for (int y = 0; y < tileLayer.getHeight(); y++) {
+                        TiledMapTileLayer.Cell cell = tileLayer.getCell(x, y);
+                        if (cell != null) {
+                            TextureRegion region = cell.getTile().getTextureRegion();
+                            TiledMapUtils.fillSplit(region);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static void fillSplit(TextureRegion[][] region) {
+        for (TextureRegion[] array : region) {
+            for (TextureRegion texture : array) {
+                fillSplit(texture);
+            }
+        }
+    }
+    public static void fillSplit(TextureRegion region) {
+        // 填充值
+        float fix = 0.01f;
+        float x = region.getRegionX();
+        float y = region.getRegionY();
+        float width = region.getRegionWidth();
+        float height = region.getRegionHeight();
+        float invTexWidth = 1f / region.getTexture().getWidth();
+        float invTexHeight = 1f / region.getTexture().getHeight();
+        region.setRegion((x + fix) * invTexWidth, (y + fix) * invTexHeight, (x + width - fix) * invTexWidth, (y + height - fix) * invTexHeight); // Trims                                                                                                                                      // region
     }
 }
