773b33c590a7ff3af57931246603edc258315ca5
WenLong <midsu_wy@163.com>
2026-06-30 16:24:48 +0800

    优化反射管理器

--- src/main/java/org/ltae/manager/ReflectionManager.java
+++ src/main/java/org/ltae/manager/ReflectionManager.java
@@ -26,7 +26,7 @@ public class ReflectionManager {
     private static Class ROOT_CLASS_GAME;
     // 引擎项目的根类
     private final static Class ROOT_CLASS_ENGINE = LtaePlugin.class;
-
+    // 单例
     private static ReflectionManager instance;
 
     // 单例限制构造器
@@ -36,7 +36,7 @@ public class ReflectionManager {
             return;
         }
     }
-    // 设置游戏项目根类
+    // 设置游戏项目根类,使用此管理器的游戏项目需要设置
     public static void setRootClass(Class<Object> ROOT_CLASS_GAME){
         ReflectionManager.ROOT_CLASS_GAME = ROOT_CLASS_GAME;
     }
@@ -63,7 +63,7 @@ public class ReflectionManager {
         return getReflections(rootClass).getSubTypesOf(type);
     }
 
-    // 获取反射工具实例
+    // 通关类来得到包含其路径与子路径内所有类的反射工具示例
     public Reflections getReflections(Class<Object> rootClass){
         URL codeSource = rootClass.getProtectionDomain().getCodeSource().getLocation();
         return new Reflections(new ConfigurationBuilder()
